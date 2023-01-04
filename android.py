4609df92fc9c5994b41b99fd1dd1e0ad11961ca7
Shahnawaz Sk <111853986+Shahnawaz-Sk@users.noreply.github.com>
2023-01-05 01:43:00 +0530

    Update android.py

--- android.py
+++ android.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
 
-build_name = os.getenv(LT_BUILD_NAME)
+build_name = os.environ.get(LT_BUILD_NAME)
 
 desired_caps = {
 	"lt:options": {
