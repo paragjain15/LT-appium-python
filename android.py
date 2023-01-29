df7135fc55efc39260e20987d593fc6291e06deb
Shahnawaz Sk <111853986+Shahnawaz-Sk@users.noreply.github.com>
2023-01-29 20:18:20 +0530

    Update android.py

--- android.py
+++ android.py
@@ -5,7 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
 
-build_name = os.environ.get("LT_BUILD_NAME")
+# build_name = os.environ.get("LT_BUILD_NAME")
+build_name = "test-app"
 # build_name = "Hello"
 
 desired_caps = {
